ad828e41596a9d3a5b7b4ba449f2f6ddb7099325
Dmytro Ostapenko <ostapenko.dmytro05@gmail.com>
2023-11-28 23:06:02 +0200

    Starting point

---
+++ Solid4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Solid4;
+
+interface IItem
+{
+    void ApplyDiscount(String discount);
+    void ApplyPromocode(String promocode);
+
+    void SetColor(byte color);
+    void SetSize(byte size);
+
+    void SetPrice(double price);
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
