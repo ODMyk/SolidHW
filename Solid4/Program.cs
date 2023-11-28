77a02ee56d2cafbbbcdde23e71cddf749f8a74ff
Dmytro Ostapenko <ostapenko.dmytro05@gmail.com>
2023-11-28 23:33:12 +0200

    4 Task

--- Solid4/Program.cs
+++ Solid4/Program.cs
@@ -2,15 +2,55 @@
 
 namespace Solid4;
 
-interface IItem
-{
-    void ApplyDiscount(String discount);
-    void ApplyPromocode(String promocode);
-
-    void SetColor(byte color);
-    void SetSize(byte size);
-
+interface IPricable {
     void SetPrice(double price);
+}
+
+interface IColorable
+{
+    void SetColor(byte color);
+}
+
+interface ISizable {
+    void SetSize(byte size);
+}
+
+interface IDiscountable {
+    void ApplyDiscount(String discount);
+}
+
+interface IPromocodable {
+    void ApplyPromocode(String promocode);
+}
+
+class Book : IPricable, IDiscountable
+{
+    public void ApplyDiscount(string discount)
+    {
+    }
+
+    public void SetPrice(double price)
+    {
+    }
+}
+
+class Clothes : ISizable, IColorable, IPricable, IDiscountable
+{
+    public void ApplyDiscount(string discount)
+    {
+    }
+
+    public void SetColor(byte color)
+    {
+    }
+
+    public void SetPrice(double price)
+    {
+    }
+
+    public void SetSize(byte size)
+    {
+    }
 }
 
 class Program
